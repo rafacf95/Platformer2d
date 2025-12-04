bd3a6163de2afb082b5943107145d51d36ff98b3
Rafael Camargo <70503254+rafacf95@users.noreply.github.com>
2025-12-04 14:13:59 -0300

    Simple enemy prototype

---
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+    public int damage = 10;
+
+    // private void OnTriggerEnter2D(Collider2D collision)
+    // {
+    //     Debug.Log(collision.transform.name);
+    //     var health = collision.GetComponent<HealthBase>();
+
+    //     if(health != null)
+    //     {
+    //         health.Damage(damage);
+    //     }
+
+    // }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.transform.name);
+        var health = collision.gameObject.GetComponent<HealthBase>();
+
+        if (health != null)
+        {
+            health.Damage(damage);
+        }
+    }
+
+}
