7a92a2fe6f7df9771a4c7771cff3f9262506fbb0
Rafael Camargo <70503254+rafacf95@users.noreply.github.com>
2025-12-04 13:19:25 -0300

    Implemented run speed for player

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -8,7 +8,10 @@ public class Player : MonoBehaviour
     public Rigidbody2D myRigidBody;
     public Vector2 friction = new Vector2(.1f, 0);
     public float speed = 5f;
+    public float speedRun = 10f;
     public float jumpForce = 10f;
+
+    private float _currentSpeed;
     private void Update()
     {
         HandleJump();
@@ -17,13 +20,21 @@ public class Player : MonoBehaviour
 
     private void HandleMovement()
     {
+
+        // if (Input.GetKey(KeyCode.LeftShift))
+        //     _currentSpeed = speedRun;
+        // else
+        //     _currentSpeed = speed;
+
+        _currentSpeed = Input.GetKey(KeyCode.LeftShift) ? speedRun : speed;
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            myRigidBody.velocity = new Vector2(-speed, myRigidBody.velocity.y);
+            myRigidBody.velocity = new Vector2(-_currentSpeed, myRigidBody.velocity.y);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            myRigidBody.velocity = new Vector2(speed, myRigidBody.velocity.y);
+            myRigidBody.velocity = new Vector2(_currentSpeed, myRigidBody.velocity.y);
         }
 
         if (myRigidBody.velocity.x > 0)
